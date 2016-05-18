e0053b4754b2a95cbeac6624e514c46dd6bc83f3
Rahpiefard <nrahpie@calstatela.edu>
2016-05-18 13:40:37 -0700

    solved some issues

--- app/src/main/java/com/android/nazli/challengeyourfriend/ContactActivity.java
+++ app/src/main/java/com/android/nazli/challengeyourfriend/ContactActivity.java
@@ -25,16 +25,6 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class ContactActivity extends AppCompatActivity {
- /*   private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }*/
-    ListView listContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,81 +32,8 @@ public class ContactActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        listContacts = (ListView) findViewById(R.id.listContact);
-        displayContacts();
-
     }
-       /* Uri queryUri = ContactsContract.Contacts.CONTENT_URI;
 
-        String[] projection = new String[]{
-                ContactsContract.Contacts._ID,
-                ContactsContract.Contacts.DISPLAY_NAME,
-
-        };
-
-
-        String selection = ContactsContract.Contacts.DISPLAY_NAME + " IS NOT NULL";
-        //String selection = ContactsContract.Contacts._ID + " IS NOT NULL";
-
-        CursorLoader cursorLoader = new CursorLoader(
-                this,
-                queryUri,
-                projection,
-                selection,
-                null,
-                null);
-
-        Cursor cursor = cursorLoader.loadInBackground();
-
-       // String[] from = {ContactsContract.Contacts.DISPLAY_NAME};
-        String[] from = {ContactsContract.Contacts._ID};
-        int[] to = {android.R.id.text1};
-
-        ListAdapter adapter = new SimpleCursorAdapter(
-                this,
-                android.R.layout.simple_list_item_1,
-                cursor,
-                from,
-                to,
-                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
-        listContacts.setAdapter(adapter);
-*/
-
-    public void displayContacts() {
-
-
-        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
-        while (phones.moveToNext())
-        {
-            // set
-            //setName(phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)));
-            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            Log.v("Phone",name+phoneNumber);
-            //Adapter
-            //what is from and to?
-            // The desired columns to be bound
-            String[] from = new String[] {
-                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                    ContactsContract.CommonDataKinds.Phone.NUMBER
-            };
-            //String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER};
-            int[] to = {android.R.id.text1};
-            ListAdapter contactAdapter=new SimpleCursorAdapter(
-                    this,
-                    android.R.layout.simple_list_item_1,
-                    phones,
-                    from,
-                    to,
-                    0);
-                    //CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
-            listContacts.setAdapter(contactAdapter);
-            //Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
-
-        }
-
-    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
--- app/src/main/java/com/android/nazli/challengeyourfriend/ContactActivityFragment.java
+++ app/src/main/java/com/android/nazli/challengeyourfriend/ContactActivityFragment.java
@@ -1,14 +1,21 @@
 package com.android.nazli.challengeyourfriend;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 
 public class ContactActivityFragment extends Fragment {
 
+    ListView contactsListView;
 
     public ContactActivityFragment() {
     }
@@ -18,8 +25,34 @@ public class ContactActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_contact, container, false);
+        contactsListView = (ListView) rootView.findViewById(R.id.listview_contacts);
+        displayContacts();
+
         return rootView;
 
     }
 
+    public void displayContacts() {
+
+        Cursor phones = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        while (phones.moveToNext())
+        {
+            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            Log.v("name and phone:", name + phoneNumber);
+            String[] from = new String[] {
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                    ContactsContract.CommonDataKinds.Phone.NUMBER
+            };
+            int[] to = {android.R.id.text1};
+            ListAdapter contactAdapter=new SimpleCursorAdapter(
+                    getActivity(),
+                    android.R.layout.simple_list_item_1,
+                    phones,
+                    from,
+                    to,
+                    0);
+            contactsListView.setAdapter(contactAdapter);
+        }
+    }
 }
