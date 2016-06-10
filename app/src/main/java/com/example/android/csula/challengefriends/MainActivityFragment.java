f2b345c6d9e74e88cdaa222d303a492ba1f183a5
SavinSachdev <savin.sachdev@gmail.com>
2016-06-10 00:53:04 -0700

    Back on activity fixed

--- app/src/main/java/com/example/android/csula/challengefriends/MainActivityFragment.java
+++ app/src/main/java/com/example/android/csula/challengefriends/MainActivityFragment.java
@@ -280,7 +280,10 @@ public class MainActivityFragment extends Fragment {
                         /* if clicked on received challenge open video sharing */
                         case 1:
                             Intent intent2 = new Intent(context, ShareActivity.class);
+                            intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                            intent2.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                             startActivity(intent2);
+
                             break;
                     }
                 }
